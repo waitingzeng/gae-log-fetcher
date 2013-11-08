e2a0a3b46830586761237e59077b9825eb7a0ae1
ttwait <ttwait@gmail.com>
2013-11-08 15:35:15 +0800

    change save to redis freq

--- redis_transport.py
+++ redis_transport.py
@@ -65,7 +65,7 @@ class BaseTransport(object):
         timestamp = self.get_timestamp(**kwargs)
         #print timestamp, line
         return self._formatters[formatter]({
-            '@source': 'file://{1}'.format(filename),
+            '@source': 'file://{0}'.format(filename),
             '@type': kwargs.get('type'),
             '@tags': kwargs.get('tags'),
             '@fields': kwargs.get('fields'),
