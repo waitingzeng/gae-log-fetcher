cf05951f122964d7d8248d53712abfa6ecc19f7a
waiting <ttwait@gmail.com>
2014-10-31 10:11:07 +0800

    fix gen

--- gen_cmd.py
+++ gen_cmd.py
@@ -10,8 +10,8 @@ GAE_TZ = tz.gettz('US/Pacific')
 
 
 for i in range(num):
-	real_start = start + i * 3600
+	real_start = (start + i * 3600) / 3600 * 3600
 	start_human = datetime.fromtimestamp(real_start, tz=GAE_TZ)
 
-	cmd = """export PYTHONPATH="/home/gae_log_fetcher/google_appengine:/home/gae_log_fetcher/google_appengine/lib/fancy_urllib";cd /home/gae_log_fetcher/current;  nohup /usr/bin/python fetcher.py --gae_config /home/gae_log_fetcher/current/fetcher.conf.agent8-backend --save_to_file /mnt/gae_logs --start_timestamp %s --end_timestamp %s > /mnt/gae_log_fetcher/%s""" % (real_start, start + 3600, start_human.strftime('%Y-%m-%d-%H'))
+	cmd = """export PYTHONPATH="/home/gae_log_fetcher/google_appengine:/home/gae_log_fetcher/google_appengine/lib/fancy_urllib";cd /home/gae_log_fetcher/current;  nohup /usr/bin/python fetcher.py --gae_config /home/gae_log_fetcher/current/fetcher.conf.agent8-backend --save_to_file /mnt/gae_logs --start_timestamp %s --end_timestamp %s > /mnt/gae_log_fetcher/%s &""" % (real_start, start + 3600, start_human.strftime('%Y-%m-%d-%H'))
 	print cmd
